276d9820ac93d3cf6bd4f0e8efe3451d2a127ce0
alakaktyc <alakaktyc@gmail.com>
2020-02-17 16:57:04 +0300

    feat: add count cats! task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    let result = [].concat(...matrix);
+    let sum = 0;
+    for(let i = 0; i < result.length; i++){
+        if (result[i] === "^^"){
+            sum += 1
+        }
+    }
+    return sum;
 };
